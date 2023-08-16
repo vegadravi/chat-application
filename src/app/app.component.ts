c2c203b9f6544fcd587b512c5325309a7e7f40cd
Ravi Vegad <rvegad@markettime.com>
2023-08-16 16:48:25 +0530

    chat application using socat and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,173 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ChatService } from './services/chat/chat.service';
+// Assuming this is the type of storageArray
+type StorageArray = Array<{
+  roomId: string;
+  chats: {
+    user: any;
+    message: string;
+  }[];
+}>;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chat-application';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('popup', { static: false }) popup: any;
+
+  public roomId: string | any;
+  public messageText: string | any;
+  public messageArray: { user: string, message: string }[] = [];
+  private storageArray: StorageArray = [];
+
+  public showScreen = false;
+  public phone: string | any;
+  public currentUser: any;
+  public selectedUser: any;
+
+  public userList = [
+    {
+      id: 1,
+      name: ' MT Ravi vegad',
+      phone: '7990021211',
+      image: 'assets/user/user-1.png',
+      roomId: {
+        2: 'room-1',
+        3: 'room-2',
+        4: 'room-3'
+      }
+    },
+    {
+      id: 2,
+      name: 'MT Ravi bhai Malani',
+      phone: '1111111111',
+      image: 'assets/user/user-2.png',
+      roomId: {
+        1: 'room-1',
+        3: 'room-4',
+        4: 'room-5'
+      }
+    },
+    {
+      id: 3,
+      name: 'chirag bhai',
+      phone: '2222222222',
+      image: 'assets/user/user-3.png',
+      roomId: {
+        1: 'room-2',
+        2: 'room-4',
+        4: 'room-6'
+      }
+    },
+    {
+      id: 4,
+      name: 'Nirav Bhai',
+      phone: '3333333333',
+      image: 'assets/user/user-4.png',
+      roomId: {
+        1: 'room-3',
+        2: 'room-5',
+        3: 'room-6'
+      }
+    }
+  ];
+
+  constructor(
+    private modalService: NgbModal,
+    private chatService: ChatService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.chatService.getMessage()
+      .subscribe((data: { user: string, room: string, message: string }) => {
+        // this.messageArray.push(data);
+        if (this.roomId) {
+          setTimeout(() => {
+            this.storageArray = this.chatService.getStorage();
+            const storeIndex = this.storageArray
+              .findIndex((storage) => storage.roomId === this.roomId);
+            this.messageArray = this.storageArray[storeIndex].chats;
+          }, 500);
+        }
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.openPopup(this.popup);
+  }
+
+  openPopup(content: any): void {
+    this.modalService.open(content, { backdrop: 'static', centered: true });
+  }
+
+  login(dismiss: any): void {
+    console.log('R4x', dismiss, this.userList);
+    this.currentUser = this.userList.find(user => user.phone === this.phone.toString());
+    this.userList = this.userList.filter((user) => user.phone !== this.phone.toString());
+
+    console.log('R4x', this.currentUser);
+    if (this.currentUser) {
+      this.showScreen = true;
+      dismiss();
+    }
+  }
+
+  selectUserHandler(phone: any): void {
+    this.selectedUser = this.userList.find(user => user.phone === phone);
+    this.roomId = this.selectedUser.roomId[this.currentUser.id];
+    this.messageArray = [];
+
+    this.storageArray = this.chatService.getStorage();
+    const storeIndex = this.storageArray
+      .findIndex((storage) => storage.roomId === this.roomId);
+
+    if (storeIndex > -1) {
+      this.messageArray = this.storageArray[storeIndex].chats;
+    }
+
+    this.join(this.currentUser.name, this.roomId);
+  }
+
+  join(username: string, roomId: string): void {
+    this.chatService.joinRoom({ user: username, room: roomId });
+  }
+
+  sendMessage(): void {
+    this.chatService.sendMessage({
+      user: this.currentUser.name,
+      room: this.roomId,
+      message: this.messageText
+    });
+
+    this.storageArray = this.chatService.getStorage();
+    const storeIndex = this.storageArray
+      .findIndex((storage) => storage.roomId === this.roomId);
+
+    if (storeIndex > -1) {
+      this.storageArray[storeIndex].chats.push({
+        user: this.currentUser.name,
+        message: this.messageText
+      });
+    } else {
+      const updateStorage = {
+        roomId: this.roomId,
+        chats: [{
+          user: this.currentUser.name,
+          message: this.messageText
+        }]
+      };
+
+      this.storageArray.push(updateStorage);
+    }
+
+    this.chatService.setStorage(this.storageArray);
+    this.messageText = '';
+  }
+
 }
